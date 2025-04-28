202ce9deb4390a982aedd8245c9ee38ba030e0bc
retosugua <renanaugusto.ras@gmail.com>
2025-04-27 22:33:51 -0300

    add interface livro

--- organo/src/app/app.component.ts
+++ organo/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, ListaLivrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'organo';
 }
---
+++ organo/src/app/components/livro/livro.ts
@@ -0,0 +1,7 @@
+export interface Livro {
+    titulo: string;
+    autoria: string;
+    imagem: string;
+    favorito: boolean;
+    genero: string;
+}
