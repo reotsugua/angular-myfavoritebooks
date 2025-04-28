3f6db64a1cad3d5585166d5533b43dba1dc5cf69
retosugua <renanaugusto.ras@gmail.com>
2025-04-28 13:38:23 -0300

    cor de genero

--- organo/src/app/components/genero-literario/genero-literario.component.ts
+++ organo/src/app/components/genero-literario/genero-literario.component.ts
@@ -1,17 +1,22 @@
 import { Component, input } from '@angular/core';
 import { LivroComponent } from '../livro/livro.component';
-import { livros } from '../../mock-livros';
 import { GeneroLiterario } from '../livro/genero-literario';
+import { UpperCasePipe } from '@angular/common';
+import { mapaCoresText } from '../../shared/mapa-cores';
 
 @Component({
   selector: 'app-genero-literario',
-  imports: [LivroComponent],
+  imports: [LivroComponent, UpperCasePipe],
   templateUrl: './genero-literario.component.html',
   styleUrl: './genero-literario.component.css'
 })
 export class GeneroLiterarioComponent {
   genero = input.required<GeneroLiterario>();
 
-  
-  
+
+  get corDoGenero() {
+    return mapaCoresText[this.genero().id]; 
+  }
+
+
 }
---
+++ organo/src/app/shared/mapa-cores.ts
@@ -0,0 +1,8 @@
+export const mapaCoresText: Record<string, string[]> = {
+    "romance": ['border-success', 'text-success'],
+    "misterio": ['border-primary', 'text-primary'],
+    "tecnicos": ['border-danger', 'text-danger'],
+    "fantasia": ['border-warning', 'text-warning'],
+    "ficcao-cientifica": ['border-dark', 'text-dark'],
+  };
+
