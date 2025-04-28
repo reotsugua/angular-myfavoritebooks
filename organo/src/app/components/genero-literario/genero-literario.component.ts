288543890987b8542856ecf322ddb602388bdc11
retosugua <renanaugusto.ras@gmail.com>
2025-04-28 00:18:49 -0300

    generos

---
+++ organo/src/app/components/genero-literario/genero-literario.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+import { LivroComponent } from '../livro/livro.component';
+import { livros } from '../../mock-livros';
+import { GeneroLiterario } from '../livro/genero-literario';
+
+@Component({
+  selector: 'app-genero-literario',
+  imports: [LivroComponent],
+  templateUrl: './genero-literario.component.html',
+  styleUrl: './genero-literario.component.css'
+})
+export class GeneroLiterarioComponent {
+  genero = input.required<GeneroLiterario>();
+
+  
+  
+}
--- organo/src/app/components/lista-livros/lista-livros.component.ts
+++ organo/src/app/components/lista-livros/lista-livros.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { LivroComponent } from '../livro/livro.component';
-import { livros } from '../../mock-livros';
+import { livros } from './../../mock-livros';
+import { Component, OnInit } from '@angular/core';
+import { GeneroLiterarioComponent } from '../genero-literario/genero-literario.component';
+import { Livro } from '../livro/livro';
+import { GeneroLiterario } from '../livro/genero-literario';
 
 @Component({
   selector: 'app-lista-livros',
-  imports: [LivroComponent],
+  imports: [GeneroLiterarioComponent],
   templateUrl: './lista-livros.component.html',
   styleUrl: './lista-livros.component.css'
 })
-export class ListaLivrosComponent {
-  livros = livros;
+export class ListaLivrosComponent implements OnInit{
+  generos: GeneroLiterario[] = [];
+  livrosPorGenero: Map<string, Livro[]> = new Map();
+
+  ngOnInit(): void {
+    livros.forEach(livro => {
+      const generoId = livro.genero.id;
+      if (!this.livrosPorGenero.has(generoId)) {
+        this.livrosPorGenero.set(generoId, [])
+      }
+      this.livrosPorGenero.get(generoId)?.push(livro)
+    });
+
+    this.generos = Array.from(this.livrosPorGenero.entries()).map(([id, livros]) => ({
+      id: id,
+      value: id, // aqui você pode mudar para o nome bonito se quiser
+      livros: livros
+    }));
+
+    
+  }
 }
